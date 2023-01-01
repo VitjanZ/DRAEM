3adbb42e356f3180e6c6843b282d5157e31f7e8b
Hamza Khalil <khalilhamza798@gmail.com>
2023-01-01 12:47:07 -0800

    Added Visualization Code

--- data_loader.py
+++ data_loader.py
@@ -60,6 +60,60 @@ class MVTecDRAEMTestDataset(Dataset):
         return sample
 
 
+class MVTecDRAEM_Test_Visual_Dataset(Dataset):
+
+    def __init__(self, root_dir, resize_shape=None):
+        self.root_dir = root_dir
+        self.images = sorted(glob.glob(root_dir+"/*/*.png"))[:2]
+        self.resize_shape=resize_shape
+
+    def __len__(self):
+        return len(self.images)
+
+    def transform_image(self, image_path, mask_path):
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        if mask_path is not None:
+            mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        else:
+            mask = np.zeros((image.shape[0],image.shape[1]))
+        if self.resize_shape != None:
+            image = cv2.resize(image, dsize=(self.resize_shape[1], self.resize_shape[0]))
+            mask = cv2.resize(mask, dsize=(self.resize_shape[1], self.resize_shape[0]))
+
+        image = image / 255.0
+        mask = mask / 255.0
+
+        image = np.array(image).reshape((image.shape[0], image.shape[1], 3)).astype(np.float32)
+        mask = np.array(mask).reshape((mask.shape[0], mask.shape[1], 1)).astype(np.float32)
+
+        image = np.transpose(image, (2, 0, 1))
+        mask = np.transpose(mask, (2, 0, 1))
+        return image, mask
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = self.images[idx]
+        dir_path, file_name = os.path.split(img_path)
+        base_dir = os.path.basename(dir_path)
+        if base_dir == 'good':
+            image, mask = self.transform_image(img_path, None)
+            has_anomaly = np.array([0], dtype=np.float32)
+        else:
+            mask_path = os.path.join(dir_path, '../../ground_truth/')
+            mask_path = os.path.join(mask_path, base_dir)
+            mask_file_name = file_name.split(".")[0]+"_mask.png"
+            mask_path = os.path.join(mask_path, mask_file_name)
+            image, mask = self.transform_image(img_path, mask_path)
+            has_anomaly = np.array([1], dtype=np.float32)
+
+        sample = {'image': image, 'has_anomaly': has_anomaly,'mask': mask, 'idx': idx}
+
+        return sample
+
+
+
 
 class MVTecDRAEMTrainDataset(Dataset):
 
